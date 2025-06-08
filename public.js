a8c0e5d0c5c40af60f1e6f923ba3bd42045fad64
gandreddi-pallavi238 <gandreedipallavi@gmail.com>
2025-06-08 11:16:25 +0530

    script.js

--- public.js
+++ public.js
@@ -3,8 +3,8 @@ const chatBox = document.getElementById("chat-box");
 const messageInput = document.getElementById("message");
 
 function sendMessage() {
-  const message = messageInput.value;
-  if (message) {
+  const message = messageInput.value.trim();
+  if (message !== "") {
     socket.emit("chat message", message);
     messageInput.value = "";
   }
